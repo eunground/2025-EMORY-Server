1f2e6dd1a2d2e58ef51c3b41e4f938da68480ade
Doyeon Kim <ehdus9809@gmail.com>
2025-06-23 23:13:43 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/emory/emoryserver/EmoryserverApplication.java
@@ -0,0 +1,13 @@
+package emory.emoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoryserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emory/emoryserver/EmoryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package emory.emoryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
